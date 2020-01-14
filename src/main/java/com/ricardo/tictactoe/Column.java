ac3064ff86bcd2f7ab4187b146a9c2e09506fde3
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-01-14 16:55:01 -0300

    Refactor Column class

--- src/main/java/com/ricardo/tictactoe/Column.java
+++ src/main/java/com/ricardo/tictactoe/Column.java
@@ -1,13 +1,18 @@
 package com.ricardo.tictactoe;
 
+import java.util.ArrayList;
+
 public class Column {
+
+    private ArrayList<Cell> cellList = new ArrayList<Cell>();
+
     public Column(int size) {
+        for(int i = 0; i <= size; i++)
+            this.cellList.add(new Cell());
     }
 
     public void putShape(IShape aShape, int position) {
-        if((position <= 3) && (position >= 0))
-            return;
-
-        throw new ArrayIndexOutOfBoundsException();
+        Cell cell = this.cellList.get(position);
+        cell.putShape(aShape);
     }
 }
