f17aaac288c9d28f7b679d2c798c6873629019d5
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-01-15 16:24:56 -0300

    Move intializer into the constructor

--- src/main/java/com/ricardo/tictactoe/Column.java
+++ src/main/java/com/ricardo/tictactoe/Column.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class Column {
 
-    private ArrayList<Cell> cellList = new ArrayList<Cell>();
+    private ArrayList<Cell> cellList;
 
     public Column(int size) {
+        this.cellList = new ArrayList<Cell>();
+
         for(int i = 0; i <= size; i++)
             this.cellList.add(new Cell());
     }
