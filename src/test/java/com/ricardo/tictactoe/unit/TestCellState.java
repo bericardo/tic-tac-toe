2ddae820351b87d63752cbaf1b7049409e8a4922
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-01-25 15:19:01 -0300

    Added CellTaken unit tests

--- src/test/java/com/ricardo/tictactoe/unit/TestCellState.java
+++ src/test/java/com/ricardo/tictactoe/unit/TestCellState.java
@@ -1,8 +1,10 @@
 package com.ricardo.tictactoe.unit;
 
 import com.ricardo.tictactoe.cell.CellFree;
+import com.ricardo.tictactoe.cell.CellTaken;
 import com.ricardo.tictactoe.cell.ICellState;
 import com.ricardo.tictactoe.cell.InvalidShapeException;
+import com.ricardo.tictactoe.shape.Circle;
 import com.ricardo.tictactoe.shape.Cross;
 import com.ricardo.tictactoe.shape.IShape;
 import org.junit.Assert;
@@ -10,34 +12,70 @@ import org.junit.Test;
 
 public class TestCellState {
 
+    // CELL FREE
     @Test
     public void testCompareReturnsAlwaysFalseWhenCellFree(){
-        ICellState cell  = new CellFree();
+        ICellState cellState  = new CellFree();
         IShape newShape = new Cross();
         IShape cellShape = null;
 
-        boolean isEqual = cell.compare(newShape, cellShape);
+        boolean isEqual = cellState.compare(newShape, cellShape);
 
         Assert.assertEquals(false, isEqual);
     }
 
     @Test (expected = InvalidShapeException.class)
     public void testPuttingNullShapeInFreeCellReturnException(){
-        ICellState cell  = new CellFree();
+        ICellState cellState  = new CellFree();
         IShape newShape = null;
         IShape cellShape = null;
 
-        cell.putShape(newShape, cellShape);
+        cellState.putShape(newShape, cellShape);
     }
 
     @Test
-    public void testWhellCellFreeAndShapeNotNullReturnNewShapeToBeStored(){
-        ICellState cell  = new CellFree();
+    public void testWhenCellFreeAndShapeNotNullReturnNewShapeToBeStored(){
+        ICellState cellState  = new CellFree();
         IShape newShape = new Cross();
         IShape cellShape = null;
 
-        IShape shapeToBeStored = cell.putShape(newShape, cellShape);
+        IShape shapeToBeStored = cellState.putShape(newShape, cellShape);
 
         Assert.assertEquals(shapeToBeStored, newShape);
+    }
+
+    //CELL TAKEN
+
+    @Test
+    public void testReturnTrueWhenShapeInCellEqualShapePassed(){
+        ICellState cellState = new CellTaken();
+        IShape newCell = new Cross();
+        IShape cellShape = new Cross();
+
+        boolean isEqual = cellState.compare(newCell, cellShape);
+
+        Assert.assertEquals(true, isEqual);
+    }
+
+    @Test
+    public void testReturnFalseWhenShapeInCellEqualShapePassed(){
+        ICellState cellState = new CellTaken();
+        IShape newCell = new Cross();
+        IShape cellShape = new Circle();
+
+        boolean isEqual = cellState.compare(newCell, cellShape);
+
+        Assert.assertEquals(false, isEqual);
+    }
+
+    @Test
+    public void testWhenCellTakenReturnShapeAlreadyStoredInCell(){
+        ICellState cellState  = new CellTaken();
+        IShape newShape = new Cross();
+        IShape cellShape = new Circle();
+
+        IShape shapeToBeStored = cellState.putShape(newShape, cellShape);
+
+        Assert.assertEquals(shapeToBeStored, cellShape);
     }
 }
