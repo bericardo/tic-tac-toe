89a1900e145c991f24bad5e24ae2bafddb92d037
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-05-30 19:34:35 -0300

    Added missed word

--- src/main/java/com/ricardo/tictactoe/Row.java
+++ src/main/java/com/ricardo/tictactoe/Row.java
@@ -12,7 +12,7 @@ public class Row extends GridElement {
      * Compares aShape with the shape stored on the cell which index is cellPos.
      * This is only used to iterate through the diagonals.
      *
-     * @param aShape    shape which will compared to the shape stored on the cell
+     * @param aShape    shape which will be compared to the shape stored on the cell
      * @param cellPos   index of the cell
      * @return          return true if the shape stored on the cell is equal to aShape
      */
