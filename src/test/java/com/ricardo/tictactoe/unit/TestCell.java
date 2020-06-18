70b76cf8f04ecc8306d9d881b879c5c74d6475a5
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-06-18 07:45:01 -0300

    Deleted and modified test cell names
    Added more descriptive names.

--- src/test/java/com/ricardo/tictactoe/unit/TestCell.java
+++ src/test/java/com/ricardo/tictactoe/unit/TestCell.java
@@ -11,78 +11,44 @@ import org.junit.Test;
 public class TestCell {
 
     @Test
-    public void testPlacingShapeInTakenCellDoesntOverrideShapeReturn() {
+    public void testShapeIsNotPlacedWhenCellIsOccupied() {
         Cell cell = new Cell();
         Shape cross = new Cross();
         Shape circle = new Circle();
         cell.putShape(cross);
 
         cell.putShape(circle);
-        boolean sameShape = cell.compare(circle);
+        boolean cellHasACircleShape = cell.compare(circle);
 
-        Assert.assertEquals(false, sameShape);
+        Assert.assertEquals(false, cellHasACircleShape);
     }
 
     @Test
-    public void testReturnTrueIfCellStoredTheShapeWePut() {
+    public void testShapeIsSuccessfulyPlacedWhenCellIsFree() {
         Cell cell = new Cell();
         Shape cross = new Cross();
 
         cell.putShape(cross);
-        boolean sameShape = cell.compare(cross);
+        boolean cellHasCrossShape = cell.compare(cross);
 
-        Assert.assertEquals(true, sameShape);
+        Assert.assertEquals(true, cellHasCrossShape);
     }
 
     @Test
-    public void testComparingAnyShapeWithEmptyCellReturnFalse() {
+    public void testCellInstanceIsIntiallyEmpty() {
         Cell cell = new Cell();
         Shape cross = new Cross();
 
-        boolean sameShape = cell.compare(cross);
+        boolean cellHasCrossShape = cell.compare(cross);
 
-        Assert.assertEquals(false, sameShape);
+        Assert.assertEquals(false, cellHasCrossShape);
     }
 
     @Test(expected = InvalidShapeException.class)
-    public void testPlacingNullShapeInFreeCellThrowException() {
+    public void testPutShapeThrowExceptionWhenShapeIsNull() {
         Cell cell = new Cell();
         Shape cross = null;
 
         cell.putShape(cross);
-    }
-
-    @Test(expected = InvalidShapeException.class)
-    public void testComparingShapeStoredInCellWithNullShapeThrowException() {
-        Cell cell = new Cell();
-        Shape cross = new Cross();
-        Shape circle = null;
-        cell.putShape(cross);
-
-        cell.compare(circle);
-    }
-
-    @Test
-    public void testShapeSettedToTrueIfCellFree() {
-        Cell cell = new Cell();
-        Shape cross = new Cross();
-
-        cell.putShape(cross);
-        boolean placed = cross.placedOnBoard();
-
-        Assert.assertEquals(true, placed);
-    }
-
-    @Test
-    public void testPlacedOnBoardReturnFalseIfCellTaken() {
-        Cell cell = new Cell();
-        Shape cross = new Cross();
-        Shape circle = new Circle();
-        cell.putShape(circle);
-        cell.putShape(cross);
-
-        boolean placed = cross.placedOnBoard();
-
-        Assert.assertEquals(false, placed);
     }
 }
